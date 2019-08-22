2564489b4433b24fed0b698481dfe9a740213ea9
Adam Hostin <xhosti02@vutbr.cz>
2019-08-22 11:33:57 +0200

    Basic screen set up main menu input processing Membership variables (Level, CurrentScreen)

--- Assets/WM2000/Hacker.cs
+++ Assets/WM2000/Hacker.cs
@@ -1,18 +1,124 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Hacker : MonoBehaviour
 {
+
+    //Game states
+    int Level=0;
+    enum screen {MainMenu,Guessing,Win };
+    screen CurrentScreen = screen.MainMenu;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ShowMainMenuScreen("Hello all mighty hacker");
     }
 
-    // Update is called once per frame
-    void Update()
+    void ShowMainMenuScreen (string Greeting)
     {
+        CurrentScreen = screen.MainMenu;
+
+        Terminal.ClearScreen();
+        Terminal.WriteLine(Greeting + "\nChoose a place where to hack:");
+        Terminal.WriteLine("Press 1 for McDonalds");
+        Terminal.WriteLine("Press 2 for Sony");
+        Terminal.WriteLine("Press 3 for White house office");
+        Terminal.WriteLine("Enter your selection:");
+    }
+
+    void ShowGuessingScreen()
+    {
+        CurrentScreen = screen.Guessing;
+
+        Terminal.ClearScreen();
+        Terminal.WriteLine("You selected level " + Level);
+        Terminal.WriteLine("Enter password:");
+
+    }
+    void ShowWinScreen()
+    {
+        CurrentScreen = screen.Win;
+
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Access granted");
+    }
+
+    void OnUserInput(string input)
+    {
+        print("The user typed " + input);
+        switch (CurrentScreen)
+        {
+            case screen.MainMenu:
+                MainMenuInputHandler(input);
+                break;
+            case screen.Guessing:
+                GuessingInputHandler(input);
+                break;
+            case screen.Win:
+                WinInputHandler(input);
+                break;
+        }
         
+            
+        
+
+    }
+
+     void WinInputHandler(string InputInWin)
+    {
+        switch (InputInWin)
+        {
+            case "menu":
+            case "Menu":
+            case "MENU":
+                ShowMainMenuScreen("Welcome back");
+                break;
+            default:
+                break;
+        }
+    }
+
+     void GuessingInputHandler(string InputInGuessing)
+    {
+        switch (InputInGuessing)
+        {
+            case "menu":
+            case "Menu":
+            case "MENU":
+                ShowMainMenuScreen("Welcome back");
+                break;
+            default:
+                Terminal.WriteLine("Access denied");
+                break;
+        }
+    }
+
+     void MainMenuInputHandler(string InputInMainMenu)
+    {
+       switch (InputInMainMenu)
+        {
+            case "1":
+            case "2":
+            case "3":
+                Int32.TryParse(InputInMainMenu, out Level);
+                ShowGuessingScreen();
+                break;
+            case "007":
+                ShowMainMenuScreen("Welcome back Mr. Bond");
+                break;
+            case "666":
+                Terminal.WriteLine("Too hot");
+                break;
+            case "3.3.":
+                Terminal.WriteLine("Happy Birthday");
+                break;
+            case "69":
+                Terminal.WriteLine("Enjoy");
+                break;
+            default:
+                break;                
+       }
     }
 }
